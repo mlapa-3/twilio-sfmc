4d1ecf0bf2c0d6dd9269548306f21c324f21e54f
mlapa-3 <85116999+mlapa-3@users.noreply.github.com>
2021-06-23 15:50:38 +0100

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.SMSAPIML.Phone}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.SMSAPIML2.To}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
